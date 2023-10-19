d4841d68e5090af806ea8d3d9aa554d552dd710a
jysaac19 <147906042+jysaac19@users.noreply.github.com>
2023-10-19 16:10:42 +0800

    Created the Home Page of the application
    - added a navcontroller at build.gradle file for navigation of screens

--- app/src/main/java/com/nbscollege/facultyevaluation/MainActivity.kt
+++ app/src/main/java/com/nbscollege/facultyevaluation/MainActivity.kt
@@ -3,13 +3,17 @@ package com.nbscollege.facultyevaluation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+
+import com.nbscollege.facultyevaluation.model.HomePage
 import com.nbscollege.facultyevaluation.ui.theme.FacultyEvaluationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +23,11 @@ class MainActivity : ComponentActivity() {
             FacultyEvaluationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = Color(0xffD22023)
                 ) {
-                    Greeting("Android ")
+                    HomePage()
                 }
             }
         }
