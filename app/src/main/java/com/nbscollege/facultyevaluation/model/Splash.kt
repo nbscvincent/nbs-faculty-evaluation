db8eab02efd932337c3beb14dbfb721ce1353976
jescanilla <147906058+jescanilla@users.noreply.github.com>
2023-11-18 11:29:42 +0800

    Update Splash
    Update Splash

--- app/src/main/java/com/nbscollege/facultyevaluation/model/Splash.kt
+++ app/src/main/java/com/nbscollege/facultyevaluation/model/Splash.kt
@@ -10,10 +10,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -44,8 +46,13 @@ fun SplashScreen(
             painter = painterResource(id = R.drawable.nbsc_logo),
             contentDescription = "NBS College",
             modifier = Modifier
-                .size(1200.dp,350.dp)
+                .size(1400.dp,350.dp)
+        )
 
+        LinearProgressIndicator(
+            modifier = Modifier,
+            color = Color.Green,
+            trackColor = Color.Gray
         )
 
     }
