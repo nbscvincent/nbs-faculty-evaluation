d46a3479e562ec0e9633658b56a89a6b65ef648b
jysaac19 <147906042+jysaac19@users.noreply.github.com>
2023-11-21 10:36:28 +0800

    Update Dashboard.kt

--- app/src/main/java/com/nbscollege/facultyevaluation/model/Dashboard.kt
+++ app/src/main/java/com/nbscollege/facultyevaluation/model/Dashboard.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Person
@@ -18,10 +20,13 @@ import androidx.compose.material.icons.rounded.Person2
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -100,30 +105,52 @@ fun Dashboard(
 
     Scaffold(
         topBar = {
-            Row {
-                Icon(imageVector = Icons.Rounded.Person2, contentDescription = "Icon Profile")
-                Text(text = "Welcome, $currentAcct")
-            }
+            TopAppBar(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(),
+
+                title ={
+                Row (
+                    modifier = Modifier
+                        .height(100.dp),
+                    horizontalArrangement = Arrangement.spacedBy(30.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Icon(imageVector = Icons.Rounded.Person2, contentDescription = "Icon Profile", tint = Color.White, modifier = Modifier.size(30.dp))
+                    Text(text = "Welcome, $currentAcct", color = Color.White, fontSize = 25.sp)
+                }
+            },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.Red
+                )
+
+            )
 
         },
         bottomBar = {
             Row (
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 20.dp)
+                    .height(50.dp)
+                    .padding(horizontal = 20.dp),
+                horizontalArrangement = Arrangement.SpaceAround
                 ){
-                    Icon(imageVector = Icons.Rounded.Home, contentDescription = "Home")
-                    Icon(imageVector = Icons.Rounded.Person, contentDescription = "Profile")
-                    Icon(imageVector = Icons.Rounded.Settings, contentDescription = "Settings")
+                    Icon(imageVector = Icons.Rounded.Home, contentDescription = "Home", tint = Color.Black, modifier = Modifier.size(30.dp))
+                    Icon(imageVector = Icons.Rounded.Person, contentDescription = "Profile", tint = Color.Black, modifier = Modifier.size(30.dp))
+                    Icon(imageVector = Icons.Rounded.Settings, contentDescription = "Settings", tint = Color.Black, modifier = Modifier.size(30.dp))
                 }
 
             }
         ) {
             Column (
-                Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
                     .background(Color.White)
+                    .padding(it)
             ){
-
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(text = "Dashboard", color = Color.Black, fontSize = 30.sp, modifier = Modifier.padding(horizontal = 20.dp))
             }
 
     }
