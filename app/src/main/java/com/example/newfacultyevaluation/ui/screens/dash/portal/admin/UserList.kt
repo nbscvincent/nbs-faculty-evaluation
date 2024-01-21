fab71b545edaa011f99cd87574c5957e9d5c827a
jescanilla <147906058+jescanilla@users.noreply.github.com>
2024-01-22 00:49:38 +0800

    Update - addtl
    Update - addtl

--- app/src/main/java/com/example/newfacultyevaluation/ui/screens/dash/portal/admin/UserList.kt
+++ app/src/main/java/com/example/newfacultyevaluation/ui/screens/dash/portal/admin/UserList.kt
@@ -1,59 +1,37 @@
 package com.example.newfacultyevaluation.ui.screens.dash.portal.admin
 
-import android.graphics.drawable.Icon
-import android.widget.CheckBox
-import android.widget.Checkable
-import android.widget.CheckedTextView
-import androidx.compose.foundation.ScrollState
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.overscroll
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.rounded.PeopleAlt
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.newfacultyevaluation.data.model.User
 import com.example.newfacultyevaluation.ui.FacultyAppViewModelProvider
-import com.example.newfacultyevaluation.ui.nav.AdminNav
-import com.example.newfacultyevaluation.ui.screens.auth.LoginViewModel
+
+
+
 
 @Composable
 fun UserList(
