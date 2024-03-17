917913be4523faab63262c229bf794d8fad5f8f1
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2024-03-17 19:17:07 +0800

    edit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -69,15 +69,15 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     //Navigation
-    val version = "2.7.4"
+    val version = "2.7.7"
     implementation("androidx.navigation:navigation-compose:$version")
     // Icons
-    implementation ("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation ("androidx.compose.material:material-icons-extended:1.6.3")
 
     val room_version = "2.6.1"
 
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
 
@@ -87,13 +87,13 @@ dependencies {
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$room_version")
 
-    val lifecycleVersion = "2.6.2"
+    val lifecycleVersion = "2.7.0"
     //Lifecycle
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
     implementation ("androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion")
 
-    val liveDataVersion = "1.5.4"
+    val liveDataVersion = "1.6.3"
     implementation ("androidx.compose.runtime:runtime-livedata:$liveDataVersion")
 
     implementation ("com.jakewharton.timber:timber:5.0.1")
@@ -111,8 +111,8 @@ dependencies {
     //Dagger - Hilt
     implementation ("com.google.dagger:hilt-android:2.45")
     ksp ("com.google.dagger:hilt-android-compiler:2.45")
-    ksp ("androidx.hilt:hilt-compiler:1.0.0")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+    ksp ("androidx.hilt:hilt-compiler:1.2.0")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation ("com.google.dagger:dagger-android-support:2.45")
 
     implementation( "io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
